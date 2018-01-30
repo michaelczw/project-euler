b63e1a5046366c75c51259d89edb0ffbb9737259
Michael Cheng <michaelchengzw@gmail.com>
2018-01-30 09:46:56 +0800

    First commit - problem 1

---
+++ problem1.js
@@ -0,0 +1,11 @@
+const findSumOfMultiples = (limit) => {
+    let sum = 0
+    for (let i = 0; i < limit; i++){
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+console.log(findSumOfMultiples(1000));
